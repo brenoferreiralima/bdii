53010670c65045cb6d7b6f5ef00064d2e5755e92
Breno Lima <brenoferreiralima@gmail.com>
2022-03-23 15:08:20 -0300

    Create atividade-01.sql

---
+++ atividade-01.sql
@@ -0,0 +1,11 @@
+/* 
+
+Baseando-se no banco de dados do hotel e usando a linguagem SQL, responda as questões abaixo.
+
+Obtenha: 
+1) O nome dos hóspedes que nasceram no ano 2000 ou que se hospedaram na data xx/yy/zzzz.
+2) O nome e a data de nascimento dos hóspedes que se hospedaram em apartamentos da categoria de código x.
+3) O nome e a data de nascimento dos hóspedes que se hospedaram em apartamentos da categoria de nome y.
+4) O código das hospedagens realizadas pelo João ou em apartamentos da categoria luxo.
+
+*/
