8334ae83834386ca07bac0bceb927a65a91b2746
Breno Lima <brenoferreiralima@gmail.com>
2022-03-23 15:08:27 -0300

    Create atividade-02.sql

---
+++ atividade-02.sql
@@ -0,0 +1,36 @@
+/*
+
+Crie e povoe o BD do Hotel. Em seguida, usando a linguagem SQL, responda as questões a seguir:
+
+01. Categorias que possuam preços entre R$ 100,00 e R$ 200,00.
+02. Categorias cujos nomes possuam a palavra ‘Luxo’.
+03. Nomes de categorias de apartamentos que foram ocupados há mais de 5 anos.
+04. Apartamentos que estão ocupados, ou seja, a data de saída está vazia.
+05. Apartamentos cuja categoria tenha código 1, 2, 3, 11, 34, 54, 24, 12.
+06. Apartamentos cujas categorias iniciam com a palavra ‘Luxo’.
+07. Quantidade de apartamentos cadastrados no sistema.
+08. Somatório dos preços das categorias.
+09. Média de preços das categorias.
+10. Maior preço de categoria.
+11. Menor preço de categoria.
+12. O preço média das diárias dos apartamentos ocupados por cada hóspede.
+13. Quantidade de apartamentos para cada categoria.
+14. Categorias que possuem pelo menos 2 apartamentos.
+15. Nome dos hóspedes que nasceram após 1° de janeiro de 1970.
+16. Quantidade de hóspedes.
+17. Apartamentos que foram ocupados pelo menos 2 vezes.
+18. Altere a tabela Hóspede, acrescentando o campo "Nacionalidade".
+19. Quantidade de hóspedes para cada nacionalidade.
+20. A data de nascimento do hóspede mais velho.
+21. A data de nascimento do hóspede mais novo.
+22. Reajuste em 10% o valor das diárias das categorias.
+23. O nome das categorias que não possuem apartamentos.
+24. O número dos apartamentos que nunca foram ocupados.
+25. O número do apartamento mais caro ocupado pelo João.
+26. O nome dos hóspedes que nunca se hospedaram no apartamento 201.
+27. O nome dos hóspedes que nunca se hospedaram em apartamentos da categoria LUXO.
+28. O nome dos hóspedes que se hospedaram ou reservaram apartamento do tipo LUXO.
+29. O nome dos hóspedes que se hospedaram mas nunca reservaram apartamentos do tipo LUXO.
+30. O nome dos hóspedes que se hospedaram e reservaram apartamento do tipo LUXO.
+
+*/
