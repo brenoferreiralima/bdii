6dd81aa94a5d6740ffe5c4e84e2ebe29a76303e9
Breno Lima <brenoferreiralima@gmail.com>
2022-04-10 14:14:06 -0300

    Update atividade-04.sql

--- atividade-04.sql
+++ atividade-04.sql
@@ -11,16 +11,16 @@
 --  1) Povoe a tabela venda com 10 vendas, considerando que existam apenas 4 vendedores na loja.
 --	   Faça com que, pelo menos, 2 vendedores empatem no somatório dos valores vendidos.
 
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (1, 'João', '2020-03-06', 200);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (2, 'João', '2020-03-18', 100);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (3, 'José', '2020-03-05', 50);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (4, 'José', '2020-03-11', 50);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (5, 'José', '2020-03-02', 50);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (6, 'José', '2020-03-30', 50);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (7, 'José', '2020-03-01', 50);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (8, 'José', '2020-03-26', 50);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (9, 'Maria', '2020-03-05', 150);
-	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (10, 'Pedro', '2020-03-28', 25);
+	INSERT INTO venda VALUES (1, 'João', '2020-03-06', 200);
+	INSERT INTO venda VALUES (2, 'João', '2020-03-18', 100);
+	INSERT INTO venda VALUES (3, 'José', '2020-03-05', 50);
+	INSERT INTO venda VALUES (4, 'José', '2020-03-11', 50);
+	INSERT INTO venda VALUES (5, 'José', '2020-03-02', 50);
+	INSERT INTO venda VALUES (6, 'José', '2020-03-30', 50);
+	INSERT INTO venda VALUES (7, 'José', '2020-03-01', 50);
+	INSERT INTO venda VALUES (8, 'José', '2020-03-26', 50);
+	INSERT INTO venda VALUES (9, 'Maria', '2020-03-05', 150);
+	INSERT INTO venda VALUES (10, 'Pedro', '2020-03-28', 25);
 
 
 --  2.1) Mostre o nome dos vendedores que venderam mais que X reais no mês de março de 2020.
