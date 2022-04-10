b387e7fd7b0b1b34361cb025022f360fd00a04d8
Breno Lima <brenoferreiralima@gmail.com>
2022-04-10 12:27:50 -0300

    Update atividade-04.sql

--- atividade-04.sql
+++ atividade-04.sql
@@ -19,3 +19,15 @@
 	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (8, 'José', '2020-03-26', 50);
 	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (9, 'Maria', '2020-03-05', 150);
 	insert into venda (cod_venda, nome_vendedor, data_venda, valor_vendido) values (10, 'Pedro', '2020-03-28', 25);
+
+--  2.1) Mostre o nome dos vendedores que venderam mais que X reais no mês de março de 2020.
+	CREATE VIEW total_vendedor AS
+	SELECT nome_vendedor AS vendedor, SUM(valor_vendido) AS total 
+	FROM venda 
+	WHERE data_venda BETWEEN '2020-03-01' AND '2020-03-31'
+	GROUP BY nome_vendedor;
+
+	SELECT vendedor FROM total_vendedor WHERE total >= 200;
+
+--  2.2) Mostre o nome de apenas um dos vendedores que mais vendeu no mês de março de 2020.
+--  3) Mostre o nome do(s) vendedor(es) que mais vendeu no mês de março de 2020. 
