550f1c1173a293f5d13471a143674930aac2f809
Breno Lima <brenoferreiralima@gmail.com>
2022-07-04 13:23:42 +0000

    Create trabalho-02.sql

---
+++ trabalho-02.sql
@@ -0,0 +1,9 @@
+-- 1) Usando subquery, obtenha o nome dos funcionários que compraram ou venderam o produto mais caro.
+-- 2) Obtenha o nome dos produtos cuja quantidade total vendida superou 5 unidades. Observe o atributo "quantidade vendida" da tabela "Item_venda".
+-- 3) Obtenha o nome dos produtos que ainda não foram vendidos.
+-- 4) Para cada nome de cliente, obtenha o valor total comprado por ele. Observe o atributo "valor total" da tabela "Venda".
+-- 5) Para cada nome de fornecedor, obtenha a quantidade de compras realizadas. Para aqueles que não possuem compras, escrever "Ainda não tem compra".
+-- 6) Obtenha o nome dos produtos que foram comprados no dia 03/03/2017, mas que não foram vendidos nesta data.
+-- 7) Sem usar subquery, obtenha o nome dos produtos que custam mais do que o produto de código 2.
+-- 8) Obtenha o nome dos clientes que compraram produtos fornecidos pelo "Armazém Tem de Tudo".
+-- 9) Obtenha o nome dos clientes que nunca compraram produtos fornecidos pelo "Armazém Tem de Tudo".
